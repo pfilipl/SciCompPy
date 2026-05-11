22b639b8b4139990bfb28622880959188092712d
Filip Baran <pfilipl@wp.pl>
2026-05-11 09:35:51 +0200

    Homework 9 ex 2 correction

--- Homeworks/ex9-2.py
+++ Homeworks/ex9-2.py
@@ -9,8 +9,7 @@ x= np.linspace(0, 1, 100)
 fig, ax = plt.subplots(1, 1)
 
 marker_size = np.sum(30*np.abs(points), 1)
-marker_color = np.full(points.shape[0], "r")
-marker_color[np.sqrt(np.sum(points**2, 1)) < 1] = "g"
+marker_color =np.where(np.sum(points**2, 1) < 1, "g", "r")
 
 ax.plot(x, np.sqrt(1-x**2), '--', color="grey")
 ax.scatter(points[:, 0], points[:, 1], marker_size, marker_color)
