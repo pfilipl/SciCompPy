c89660367a617721d53d45f9afec345096b91203
Filip Baran <pfilipl@wp.pl>
2026-05-16 16:39:02 +0200

    Homework 10 ex 3 correction

--- Homeworks/ex10-3.py
+++ Homeworks/ex10-3.py
@@ -23,3 +23,20 @@ periodic_table = pd.DataFrame(
     )
 
 print(periodic_table)
+
+
+"""
+Result:
+
+        Name Symbol Weight                                                                                                                                                                 
+1   Hydrogen      H      1
+2     Helium     He      4
+3    Lithium     Li      7
+4   Berylium     Be      9
+5      Boron      B     11
+6     Carbon      C     12
+7   Nitrogen      N     14
+8     Oxygen      O     16
+9   Fluorine      F     19
+10      Neon     Ne     20
+"""
