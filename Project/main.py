a9850dda3dc81a6252b78b3efdd459d7637e7e6c
Filip Baran <pfilipl@wp.pl>
2026-06-09 23:05:41 +0200

    Adding Detector class

--- Project/main.py
+++ Project/main.py
@@ -9,8 +9,9 @@ Copyright (C) 2026 Filip J. Baran
 
 # --- libraries --- #
 import sys
-import argparse
 import xraylib
+from numpy import sqrt
+from argparse import ArgumentParser
 from PySide6 import QtWidgets, QtCore
 
 # --- own files --- #
@@ -33,28 +34,41 @@ class MainWindow(QtWidgets.QMainWindow):
 
         super().__init__(parent)
 
+        # --- command line arguments --- #
         try:
-            energy = float(args.energy)
+            energy = float(args.energy) / 1000  # [eV] -> [keV] for xraylib
         except Exception:
             energy = None
 
+        # --- variables --- #
+        self.Detectors = {"SDD1": Detector(), "SDD2": Detector()}
+
+        # --- --- layout --- --- #
         self.setWindowTitle("SciCompPy project app: XRF Spectra ROI Definer")
         self.setFixedSize(1000, 600)
 
+        # --- central widget --- #
         mainWidget = QtWidgets.QTabWidget(
             tabPosition=QtWidgets.QTabWidget.TabPosition.West
         )
-        xrfTab = periodic_table.PeriodicTables(
-            {
-                "Kα": {"line": xraylib.KA_LINE, "edge": xraylib.K_SHELL},
-                "Kβ": {"line": xraylib.KB_LINE, "edge": xraylib.K_SHELL},
-                "Lα": {"line": xraylib.LA_LINE, "edge": xraylib.L3_SHELL},
-                "Lβ": {"line": xraylib.LB_LINE, "edge": xraylib.L2_SHELL},
-                "Mα1": {"line": xraylib.MA1_LINE, "edge": xraylib.M5_SHELL},
-            },
-            energy,
+        self.setCentralWidget(mainWidget)
+
+        # --- XRF tab --- #
+        self.xrfTab = mainWidget.addTab(
+            periodic_table.PeriodicTables(
+                {
+                    "Kα": {"line": xraylib.KA_LINE, "edge": xraylib.K_SHELL},
+                    "Kβ": {"line": xraylib.KB_LINE, "edge": xraylib.K_SHELL},
+                    "Lα": {"line": xraylib.LA_LINE, "edge": xraylib.L3_SHELL},
+                    "Lβ": {"line": xraylib.LB_LINE, "edge": xraylib.L2_SHELL},
+                    "Mα1": {"line": xraylib.MA1_LINE, "edge": xraylib.M5_SHELL},
+                },
+                energy,
+            ),
+            "XRF lines",
         )
-        self.xrfTab = mainWidget.addTab(xrfTab, "XRF lines")
+
+        # --- manual tab --- #
         self.manualTab = mainWidget.addTab(
             QtWidgets.QLabel(
                 "Temporary widget", alignment=QtCore.Qt.AlignmentFlag.AlignCenter
@@ -62,7 +76,71 @@ class MainWindow(QtWidgets.QMainWindow):
             "Manual",
         )
 
-        self.setCentralWidget(mainWidget)
+
+class Detector:
+    """ 
+    Detector object for energetic callibration properties storage.
+    """
+
+    def __init__(
+        self, zero=-647.684, gain=6.953, noise=140, fano=0.006, epsilon=3.85, N=4096
+    ):
+        """ 
+        Object initialization with default callibration values.
+
+        It sets callibration parameters:
+        'zero' [eV], 'gain' [eV/channel], 'noise' [eV], and 'fano' [-];
+        and detector parameters:
+        'epsilon' [eV], and 'N' (number of channels) [-].
+        """
+
+        self.calibrate(zero, gain, noise, fano)
+        self.epsilon = epsilon  # [eV]
+        self.N = N  # [-]
+
+    def calibrate(self, zero, gain, noise, fano):
+        """ 
+        Callibration method.
+
+        It sets callibration parameters:
+        'zero' [eV], 'gain' [eV/channel], 'noise' [eV], and 'fano' [-].
+        """
+
+        self.zero = zero  # [eV]
+        self.gain = gain  # [eV/ch]
+        self.noise = noise  # [eV]
+        self.fano = fano  # [-]
+
+    def get_energy(self, channel):
+        """ 
+        Energy getter method.
+
+        It returns energy [eV] for specified detector's channel.
+        """
+
+        return channel * self.gain + self.zero
+
+    def get_sigma(self, energy=None, *, channel=None):
+        """ 
+        Standard deviation getter method.
+
+        It returns standard deviation (sigma) [eV] for specified energy [eV], or channel.
+        """
+
+        if (energy is None) and (channel is not None):
+            energy = channel * self.gain + self.zero
+        return (
+            sqrt(self.noise ^ 2 + 2.355 ^ 2 * self.epsilon * self.fano * energy) / 2.335
+        )
+
+    def get_channel(self, energy):
+        """ 
+        Channel getter method.
+
+        It returns detector's channel for specified energy [eV].
+        """
+
+        return round((energy - self.zero) / self.gain)
 
 
 # --- --- EXECUTABLE --- --- #
@@ -70,16 +148,14 @@ class MainWindow(QtWidgets.QMainWindow):
 
 # --- running the application --- #
 def main():
-    parser = argparse.ArgumentParser(
+    parser = ArgumentParser(
         prog="XRF Spectra ROI Definer",
         description="""
         SciCompPy project application for defining energetic regions of interest (ROIs) 
         for X-ray Fluorescence spectra analysis.
         """,
     )
-    parser.add_argument(
-        "-e", "--energy", required=False, help="excitation energy [keV]"
-    )
+    parser.add_argument("-e", "--energy", required=False, help="excitation energy [eV]")
     args = parser.parse_args()
 
     app = QtWidgets.QApplication(sys.argv)
