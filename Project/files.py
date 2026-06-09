64bb2a2e2dc86e1e0cfe91a3ca90c86e97e2b5b9
Filip Baran <pfilipl@wp.pl>
2026-06-09 23:07:36 +0200

    Preparing template files for form widget and files handling

---
+++ Project/files.py
@@ -0,0 +1,32 @@
+"""
+Files module for *SciCompPy project application*.
+
+It defines input and output files handling.
+
+---
+Copyright (C) 2026 Filip J. Baran
+"""
+
+# --- --- IMPORTS --- --- #
+
+# --- libraries --- #
+import xraylib
+from PySide6 import QtWidgets, QtCore
+
+# --- own files --- #
+import main
+
+
+# --- --- CODE --- --- #
+
+
+# --- classes --- #
+class Files(QtWidgets.QWidget):
+    pass
+
+
+# --- --- EXECUTABLE --- --- #
+
+# --- running the application --- #
+if __name__ == "__main__":
+    main.main()
---
+++ Project/form.py
@@ -0,0 +1,32 @@
+"""
+Form module for *SciCompPy project application*.
+
+It defines form widget with necessary functionality.
+
+---
+Copyright (C) 2026 Filip J. Baran
+"""
+
+# --- --- IMPORTS --- --- #
+
+# --- libraries --- #
+import xraylib
+from PySide6 import QtWidgets, QtCore
+
+# --- own files --- #
+import main
+
+
+# --- --- CODE --- --- #
+
+
+# --- classes --- #
+class Form(QtWidgets.QWidget):
+    pass
+
+
+# --- --- EXECUTABLE --- --- #
+
+# --- running the application --- #
+if __name__ == "__main__":
+    main.main()
